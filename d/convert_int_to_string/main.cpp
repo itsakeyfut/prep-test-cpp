164241f6efa0569d8c9c686ce4a147f3ab832baf
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-04-17 01:47:33 +0900

    memo

---
+++ d/convert_int_to_string/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double d;
+    int n;
+    cin >> d >> n;
+    string str = "%.";
+    str += to_string(n);
+    str += "f\n";
+    printf(str.c_str(), d);
+
+    return 0;
+}
